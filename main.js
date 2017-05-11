d38a2b00f5df4cf8ebfaa91319324d26b62a9ab9
Karim <aem@ascora.de>
2017-05-11 14:39:16 +0200

    don't assign remotes id explicitly

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
                 // the id/element dom element that will hold "our" video
                 localVideoEl: 'localVideo',
                 // the id/element dom element that will hold remote videos
-                remoteVideosEl: 'remotes',
+                remoteVideosEl: '',
                 // immediately ask for camera access
                 autoRequestMedia: true,
                 debug: true,
