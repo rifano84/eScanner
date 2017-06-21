3122a26069392feb1edfef9860345c9f57d31521
Karim <aem@ascora.de>
2017-06-21 13:47:52 +0200

    add TURN definition

--- webrtc.js
+++ webrtc.js
@@ -12,7 +12,19 @@ function WebRTC(opts) {
             debug: false,
             // makes the entire PC config overridable
             peerConnectionConfig: {
-                 iceServers: [{'urls': 'stun:stun2.l.google.com:19302'}]
+                 iceServers: [{'url': 'stun:stun2.l.google.com:19302'},
+                 {
+      'url': 'turn:192.158.29.39:3478?transport=udp',
+      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      'username': '28224511:1379330808'
+    },
+    {
+      'url': 'turn:192.158.29.39:3478?transport=tcp',
+      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      'username': '28224511:1379330808'
+    }
+                 ]
+                //   iceServers: [{'urls': 'stun:stun2.l.google.com:19302'}]
                 // iceServers: [{'urls': 'stun:stun.l.google.com:19302'}]
             },
             peerConnectionConstraints: {
